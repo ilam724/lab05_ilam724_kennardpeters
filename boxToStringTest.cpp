8a81011856a973e37ba7b6419dbe0e46cc884961
Kennard Peters <kennardpeters@umail.ucsb.edu>
2018-02-23 15:32:01 -0800

    proofread boxToStringTest to test file

--- boxToStringTest.cpp
+++ boxToStringTest.cpp
@@ -10,15 +10,13 @@
 
 
 
-
 int main() {
 
   struct Box b1,b2;
-  struct Box b1Expected;
-  initBox(&b1,-5.0,6.0,1.0,2.0);
-  b1Expected.ul.x = -5.0;  b1Expected.ul.y = 6.0;
-  b1Expected.width = 1.0;  b1Expected.height = 2.0;
-  assertTrue(boxesApproxEqual(b1,b1Expected),"boxesApproxEqual(b1,b1Expected)");
+  initBox(&b1,-5.1234567,6.234567,1.145678,2.12456778);
+  assertEquals("[ul=(-5, 6), w=1, h=2]", boxToString(b1,1), "boxToString(b1,1)");
+  assertEquals("[ul=(-5.1, 6.2), w=1.1, h=2.1]", boxToString(b1,2), "boxToString(b1,2)");
+  assertEquals("[ul=(-5.12, 6.23), w=1.15, h=2.12]", boxToString(b1,3), "boxToString(b1,3)");
 
   initBox(&b2, 8.23459466, 9.12892831853071, 4.563453463453445, 5.3646793456);
   assertEquals("[ul=(8.23, 9.13), w=4.56, h=5.36]", boxToString(b2), "boxToString(b2)");
